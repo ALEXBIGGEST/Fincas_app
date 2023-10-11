17ee24eb7453c0e2364de81ca246ca0e1957cc48
saneidy <holgerandresmj@hotmail.com>
2023-10-11 08:40:51 -0500

    Ajustando variables y creación del getter

--- Código Finca/cls_registro_finca.java
+++ Código Finca/cls_registro_finca.java
@@ -1,21 +1,34 @@
 public class cls_registro_finca{
 
-    private String str_nombre, str_extension, str_propietario, str_direccion, str_ubicacion;
-    private int int_codigo;
+    private String str_nombre, str_extension, str_propietario, str_direccion, str_ubicacion,
+    str_actividad, str_codigo;
     private float flt_produccion;
 
     public cls_registro_finca(String nombre, String extension, String propietario, String direccion, 
-    String ubicacion, int codigo, float produccion) {
+    String ubicacion, String codigo, float produccion, String actividad) {
 
         this.str_nombre = nombre;
         this.str_extension = extension;
         this.str_propietario = propietario;
         this.str_direccion = direccion;
         this.str_ubicacion = ubicacion;
-        this.int_codigo = codigo;
+        this.str_codigo = codigo;
         this.flt_produccion = produccion;
+        this.str_actividad = actividad;
 
 
     }
+
+
+    //Getters
+
+    public String getNombre(){return str_nombre;}
+    public String getExtension(){return str_extension;}
+    public String getPropietario(){return str_propietario;}
+    public String getDireccion(){return str_direccion;}
+    public String getUbicacion(){return str_ubicacion;}
+    public String getCodigo(){return str_codigo;}
+    public float getProduccion(){return flt_produccion;}
+    public String getActividad(){return str_actividad;}
 
 }
