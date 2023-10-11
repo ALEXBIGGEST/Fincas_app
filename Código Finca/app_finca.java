a2d5ab6f03cdffaedb969ce4d4d0d5fe1d599b94
Yorlenis Córdoba Ayala <145945151+Yorle09@users.noreply.github.com>
2023-10-11 09:48:24 -0500

    actuaizar

--- Código Finca/app_finca.java
+++ Código Finca/app_finca.java
@@ -16,7 +16,7 @@ public class app_finca{
         String propietario = JOptionPane.showInputDialog(null, "Dueño de la finca");
         String direccion = JOptionPane.showInputDialog(null, "Dirección de la finca");
         String ubicacion = JOptionPane.showInputDialog(null, "Ubicación de la finca");
-        float produccion = Float.parseFloat(JOptionPane.showInputDialog(null, "Producción de la finca"));
+        String produccion = JOptionPane.showInputDialog(null, "Producción de la finca");
         String actividad = JOptionPane.showInputDialog(null, "Actividad económica de la finca");
         fincas[int_posiciones] = new cls_registro_finca(nombre, extension, propietario, direccion, ubicacion, codigo, produccion, actividad);
 
