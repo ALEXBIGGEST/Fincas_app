cc18f869434c84dfecfd1b05847374b3dc9da3a8
saneidy <holgerandresmj@hotmail.com>
2023-10-11 08:30:39 -0500

    Agregando variables

---
+++ Código Finca/cls_registro_finca.java
@@ -0,0 +1,8 @@
+public class cls_registro_finca{
+
+    private String str_nombre, str_extension, str_propietario, str_direccion, str_ubicación;
+    private int int_codigo;
+    private float flt_produccion;
+
+
+}
