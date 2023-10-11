ef454b41578f8a15596663777d258dba44494465
Holger Andres Meneses Jaramillo <holgerandresmj@hotmail.com>
2023-10-11 09:47:25 -0500

    actualizar

--- Código Finca/cls_registro_finca.java
+++ Código Finca/cls_registro_finca.java
@@ -1,11 +1,11 @@
 public class cls_registro_finca{
 
     private String str_nombre, str_extension, str_propietario, str_direccion, str_ubicacion,
-    str_actividad, str_codigo;
-    private float flt_produccion;
+    str_actividad, str_codigo, str_produccion;
+    
 
     public cls_registro_finca(String nombre, String extension, String propietario, String direccion, 
-    String ubicacion, String codigo, float produccion, String actividad) {
+    String ubicacion, String codigo, String produccion, String actividad) {
 
         this.str_nombre = nombre;
         this.str_extension = extension;
@@ -13,7 +13,7 @@ public class cls_registro_finca{
         this.str_direccion = direccion;
         this.str_ubicacion = ubicacion;
         this.str_codigo = codigo;
-        this.flt_produccion = produccion;
+        this.str_produccion = produccion;
         this.str_actividad = actividad;
 
 
@@ -28,7 +28,7 @@ public class cls_registro_finca{
     public String getDireccion(){return str_direccion;}
     public String getUbicacion(){return str_ubicacion;}
     public String getCodigo(){return str_codigo;}
-    public float getProduccion(){return flt_produccion;}
+    public String getProduccion(){return str_produccion;}
     public String getActividad(){return str_actividad;}
 
     //Setters
@@ -37,7 +37,7 @@ public class cls_registro_finca{
     public void setPropietario(String propietario){this.str_propietario = propietario;}
     public void setDireccion(String direccion){this.str_direccion = direccion;}
     public void setUbicacion(String ubicacion){this.str_ubicacion = ubicacion;}
-    public void setProduccion(float produccion){this.flt_produccion = produccion;}
+    public void setProduccion(String produccion){this.str_produccion = produccion;}
     public void setActividad(String actividad){this.str_actividad = actividad;}
     
 
