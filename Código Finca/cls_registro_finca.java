31f0d7d504c1907ed5bd81163604cc1925f452b3
saneidy <holgerandresmj@hotmail.com>
2023-10-11 08:34:38 -0500

    Actualización

--- Código Finca/cls_registro_finca.java
+++ Código Finca/cls_registro_finca.java
@@ -1,8 +1,21 @@
 public class cls_registro_finca{
 
-    private String str_nombre, str_extension, str_propietario, str_direccion, str_ubicación;
+    private String str_nombre, str_extension, str_propietario, str_direccion, str_ubicacion;
     private int int_codigo;
     private float flt_produccion;
 
+    public cls_registro_finca(String nombre, String extension, String propietario, String direccion, 
+    String ubicacion, int codigo, float produccion) {
+
+        this.str_nombre = nombre;
+        this.str_extension = extension;
+        this.str_propietario = propietario;
+        this.str_direccion = direccion;
+        this.str_ubicacion = ubicacion;
+        this.int_codigo = codigo;
+        this.flt_produccion = produccion;
+
+
+    }
 
 }
